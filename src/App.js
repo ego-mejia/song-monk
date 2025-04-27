7c51690f3ae4b63efbb3b761cc7cc5ba66b24a51
Fernando Mejia Laguna <mejia190899@gmail.com>
2025-04-27 09:04:57 -0600

    Created Header component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import "./App.css";
-// Este será el componente raíz. Importa y usa los componentes Header y Song.
-// Muestra al menos tres canciones en la pantalla usando múltiples instancias del componente Song.
-
-// Estilizar la app
-// Aplica estilos básicos con CSS para que la interfaz sea clara
-//  y fácil de leer. Puedes usar un archivo App.css para esto.
-
-// Imprimir un mensaje con componentDidMount
-
-// En el componente App, implementa el método componentDidMount
-// para mostrar un mensaje en la consola indicando que la app se ha cargado correctamente.
+import { Header } from "./components/Header";
+import "./style/main.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Header />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,22 @@
 // Crea un componente de clase que muestra un encabezado con el título de la app.
+import React, { Component } from "react";
+import logo from "../assets/logo-large.svg";
+
+export class Header extends Component {
+  render() {
+    return (
+      <header className="navbar">
+        <a className="navbar__img">
+          <img src={logo} alt="" />
+        </a>
+        <div className="navbar__links">
+          <a href="">Inicio</a>
+          <a href="">Nuevo</a>
+          <a href="">Newsletter</a>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
