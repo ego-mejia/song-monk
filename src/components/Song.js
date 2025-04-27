d40e395a72b88fcd3b8f9de300617e7778342ea5
Fernando Mejia Laguna <mejia190899@gmail.com>
2025-04-27 09:08:44 -0600

    Created song component

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,1 +1,21 @@
 // Crea un componente de clase que recibe props para mostrar la información de una canción (título, artista, duración).
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export class Song extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: "Good Good",
+      artist: "Usher, Summer Walker & 21 Savage",
+      album: "Coming Home",
+    };
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Song;
