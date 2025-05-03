6b06c7c020e630c57da01fd65eb24e07d1d8569f
Fernando Mejia Laguna <mejia190899@gmail.com>
2025-05-03 09:43:49 -0600

    Added react hooks for fetching data and update songlist state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Header from "./components/Header";
 import Songs from "./components/SearchResults";
 import Library from "./components/Library";
@@ -6,31 +6,32 @@ import { songsList } from "./components/songsData";
 
 import "./style/main.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+// console.log("La app está lista para Monkear!🤘");
+// songsList.slice(0, Math.floor(songsList.length / 2))
 
-    this.state = {
-      allSongs: songsList,
-      librarySongs: songsList.slice(0, Math.floor(songsList.length / 2)),
+const App = () => {
+  const [allSongs, setallSongs] = useState([]);
+  const [library, setLibrary] = useState([]);
+
+  useEffect(() => {
+    const fetchSongs = () => {
+      console.log("fetching data...");
+      setallSongs(songsList);
+      console.log("La app está lista para Monkear!🤘");
     };
-  }
+    fetchSongs();
+  }, []);
 
-  componentDidMount() {
-    console.log("La app está lista para Monkear!🤘");
-  }
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <div className="App__title">
-          <h1>Ponte Monk!</h1>
-        </div>
-        <Songs songList={this.state.allSongs} />
-        <Library songList={this.state.librarySongs} />
+  return (
+    <div className="App">
+      <Header />
+      <div className="App__title">
+        <h1>Ponte Monk!</h1>
       </div>
-    );
-  }
-}
+      {/* <Songs songList={this.state.allSongs} />
+      <Library songList={this.state.librarySongs} /> */}
+    </div>
+  );
+};
 
 export default App;
