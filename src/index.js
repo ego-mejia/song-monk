fa25eaaa36c6b572110fb37315438783554ddfb2
Fernando Mejia Laguna <mejia190899@gmail.com>
2025-09-06 17:01:16 -0600

    created store and added to index file

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./style/index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
