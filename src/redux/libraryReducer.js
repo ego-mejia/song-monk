b8730579692b72e007dfb019389b99dab4b16f41
Fernando Mejia Laguna <mejia190899@gmail.com>
2025-09-06 17:08:50 -0600

    created remove_song reducer

---
+++ src/redux/libraryReducer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const initialState = {
+  songs: [],
+};
+/* 
+    *Song Properties
+    !! CHECK ALBUM COMPONENT!
+*/
+const libraryReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_SONG": //Agrega una canción al estado de la biblioteca
+      return {}; //!PENDING
+    case "REMOVE_SONG": //Elimina una canción del estado de la biblioteca según su ID
+      return {
+        ...state,
+        songs: state.songs.filter((song) => song.id !== action.payload),
+      };
+  }
+};
+
+export default libraryReducer;
