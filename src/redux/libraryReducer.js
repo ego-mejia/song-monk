99c91b61fd33d6ead8487282a648110a677d96ac
Fernando Mejia Laguna <mejia190899@gmail.com>
2025-09-07 06:19:57 -0600

    added feature that doesn't allows duplicate values in Library

--- src/redux/libraryReducer.js
+++ src/redux/libraryReducer.js
@@ -16,8 +16,17 @@ const libraryReducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_ALBUM": //Agrega una canción al estado de la biblioteca
       // Si ya existe, no se agrega a favoritos
+      const albumExists = state.albums.some(
+        //* iterar en un array de objetos
+        (album) => album.id === action.payload.id
+      );
+
+      if (albumExists) {
+        return state;
+      }
 
       return { ...state, albums: [...state.albums, action.payload] };
+
     case "REMOVE_ALBUM": //Elimina una canción del estado de la biblioteca según su ID
       return {
         ...state,
