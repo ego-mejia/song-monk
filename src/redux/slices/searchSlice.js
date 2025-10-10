33e1d790f000e55c4899c80f05329737c16d3eda
Fernando Mejia <103783151+ego-mejia@users.noreply.github.com>
2025-10-10 18:02:38 +0000

    added AsyncThunk for search slice

--- src/redux/slices/searchSlice.js
+++ src/redux/slices/searchSlice.js
@@ -1,15 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
 // results: Array para almacenar las canciones obtenidas de la API.
 // loading: Booleano para indicar si la app está cargando.
 // error: Mensaje de error, si ocurre.
-    results: [],
-    loading: false,
-    error: '',
+    results: [], //Obtainer Albums from API.
+    loading: false, //Indicates if app is loading
+    error: '', //error message
 
 }
 
+// Asunc Thunk: albums search
+export const fetchAlbums = createAsyncThunk(
+  'search/fetchSongs',
+  async (idArtist, { rejectWithValue }) => {
+    try{
+        // Fetch from TheAudioDB API
+        const response = await axios.get(
+            `https://www.theaudiodb.com/api/v1/json/2/album.php?i=${idArtist}`
+        );
+
+    if(!response.data.album) {
+            return [];
+        }
+    
+    return response.data.album;
+    } catch (error) {
+        return rejectWithValue(error.message || 'Error al obtener los álbumes');
+    }
+
+  }  
+);
+
+
+// Main Slice
 const searchSlice = createSlice({
     name: 'search',
     initialState,
